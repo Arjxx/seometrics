ce6293937cec35107897b1369b13af16b9a9847f
Arjxx <arjunmultimedia@gmail.com>
2023-08-18 15:39:58 +0530

    how to check html

--- google-index.php
+++ google-index.php
@@ -1,135 +1,181 @@
 <?php include 'header.php' ?>
 </div>
 		<!-- end header pages -->
-		<!-- features -->
+		 <!-- feature start -->
+		<!-- <div class="grank">	</div> -->
 		<div class="container">
-				<div class="titlee text-center  p-5">
-				   <img src="./img/google.png" alt="" class="googlelogo">
-				   <div>						
-					  <span class="pt-2 ">Discover Your Website's Google Footprint with our</span>
-				   </div>
-				   <h2 class=" featurehead pt-1 ">Google Index Checker and Google Rank Checker</h2>
-				   <div>
-					  <i class="fas fa-check text-secondary me-2 pr-2"></i>Lorem ipsum 
-					  <i class="fas fa-check text-secondary me-2 pr-2"></i>Lorem ipsum 
-					  <i class="fas fa-check text-secondary me-2 pr-2"></i>Lorem ipsum 
-					  </span>
-				   </div>
-				   <span> 
-				   <button class="button">Check Now</button>
-				</div>
-				<span class="headspan text-center">Key Features</span>
-				<div class="row  text-md-start">
-				   <div class="col-md-6 ">
-					  <h2 class="gindex">Google Index Checker</h2>
-					  <ul class="list-unstyled  text-muted mt-3">
-						 <li class="mb-3">
-							<i class="fas fa-check text-success me-2 pr-2"></i>Instantaneous Results: No waiting, know instantly the indexed pages.
-						 </li>
-						 <li class="mb-3">
-							<i class="fas fa-check text-success me-2 pr-2"></i>Deep Insights: Understand potential indexation issues and act promptly.
-						 </li>
-						 <li class="mb-3">
-							<i class="fas fa-check text-success me-2 pr-2"></i>Bulk URL Checks: Verify multiple URLs at once to save time.
-						 </li>
-						 <li class="mb-3">
-							<i class="fas fa-check text-success me-2 pr-2" ></i>Easy theming and customization
-						 </li>
-					  </ul>
-				   </div>
-				   <div class="col-md-6">
-					  <h2 class="gindex">Google Ranking Checker</h2>
-					  <ul class="list-unstyled  text-muted mt-3">
-						 <li class="mb-3">
-							<i class="fas fa-check text-success me-2 pr-2"></i>Pinpoint Precision: Identify your website's exact standing for any keyword.
-						 </li>
-						 <li class="mb-3">
-							<i class="fas fa-check text-success me-2 pr-2"></i>Competitive Analysis: Measure your site's rank against competitors.
-						 </li>
-						 <li class="mb-3">
-							<i class="fas fa-check text-success me-2 pr-2"></i>Tailored Recommendations: Tips for climbing higher in search results.
-						 </li>
-						 <li class="mb-3">
-							<i class="fas fa-check text-success me-2 pr-2" ></i>Easy theming and customization
-						 </li>
-					  </ul>
-				   </div>
 
-				   <div id="accordion">
-					  <div class="card">
-						 <div class="card-header" id="headingOne">
-							<h5 class="mb-0">
-							   <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-							   Collapsible Group Item #1
-							   </button>
-							</h5>
-						 </div>
-						 <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-							<div class="card-body">
-							   Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-							</div>
-						 </div>
-					  </div>
-					  <div class="card">
-						 <div class="card-header" id="headingTwo">
-							<h5 class="mb-0">
-							   <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-							   Collapsible Group Item #2
-							   </button>
-							</h5>
-						 </div>
-						 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-							<div class="card-body">
-							   Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-							</div>
-						 </div>
-					  </div>
-					  <div class="card">
-						 <div class="card-header" id="headingThree">
-							<h5 class="mb-0">
-							   <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-							   Collapsible Group Item #3
-							   </button>
-							</h5>
-						 </div>
-						 <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-							<div class="card-body">
-							   Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-							</div>
-						 </div>
-					  </div>
-					  
-					 
-					<div class="container">
-						 <div class="row align-items-center">
-							<div class="col-md-4 text-center ">
-							   <div class="entry" data-aos="zoom-in" data-aos-duration="2000">
-								  <i class="far fa-user-circle"></i>
-								  <p>Lorem ipsum dolor sit consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper</p>
-								  <h5>John Doe</h5>
-							   </div>
-							</div>
-							<div class="col-md-4 text-center">
-							   <div class="entry" data-aos="zoom-in" data-aos-duration="2000">
-								  <i class="far fa-user-circle"></i>
-								  <p>Lorem ipsum dolor sit consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper</p>
-								  <h5>Ngolo</h5>
-							   </div>
-							</div>
-							<div class="col-md-4 text-center">
-							   <div class="entry" data-aos="zoom-in" data-aos-duration="2000">
-								  <i class="far fa-user-circle"></i>
-								  <p>Lorem ipsum dolor sit consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper</p>
-								  <h5>Konate</h5>
-							   </div>
-							</div>
-						 </div>
-					</div>
+<div class="titlee text-center  p-5">
+   <img src="./img/google.png" alt="" class="googlelogo">
+   <div>						
+	  <span class="pt-2 ">Discover Your Website's Google Footprint with our</span>
+   </div>
+   <h1 class=" featurehead pt-1 ">Google Index Checker and Google Rank Checker</h1>
+   <div>
+	  <i class="fas fa-check text-secondary me-2 pr-2"></i>Lorem ipsum 
+	  <i class="fas fa-check text-secondary me-2 pr-2"></i>Lorem ipsum 
+	  <i class="fas fa-check text-secondary me-2 pr-2"></i>Lorem ipsum 
+	  </span>
+   </div>
+   <span> 
+   <button class="button">Check Now</button>
+</div>
+<h2 class="headspan text-center">Key Features</h2>
+<div class="row  text-md-start">
+<div class="col-md-6 ">
+   <h3 class="gindex">Google Index Checker</h3>
+   <ul class="list-unstyled  text-muted mt-3">
+	  <li class="mb-3">
+		 <i class="fas fa-check text-success me-2 pr-2"></i>Instantaneous Results: No waiting, know instantly the indexed pages.
+	  </li>
+	  <li class="mb-3">
+		 <i class="fas fa-check text-success me-2 pr-2"></i>Deep Insights: Understand potential indexation issues and act promptly.
+	  </li>
+	  <li class="mb-3">
+		 <i class="fas fa-check text-success me-2 pr-2"></i>Bulk URL Checks: Verify multiple URLs at once to save time.
+	  </li>
+	  <li class="mb-3">
+		 <i class="fas fa-check text-success me-2 pr-2" ></i>Easy theming and customization
+	  </li>
+   </ul>
+</div>
+<div class="col-md-6">
+   <h3 class="gindex">Google Ranking Checker</h3>
+   <ul class="list-unstyled  text-muted mt-3">
+	  <li class="mb-3">
+		 <i class="fas fa-check text-success me-2 pr-2"></i>Pinpoint Precision: Identify your website's exact standing for any keyword.
+	  </li>
+	  <li class="mb-3">
+		 <i class="fas fa-check text-success me-2 pr-2"></i>Competitive Analysis: Measure your site's rank against competitors.
+	  </li>
+	  <li class="mb-3">
+		 <i class="fas fa-check text-success me-2 pr-2"></i>Tailored Recommendations: Tips for climbing higher in search results.
+	  </li>
+	  <li class="mb-3">
+		 <i class="fas fa-check text-success me-2 pr-2" ></i>Easy theming and customization
+	  </li>
+   </ul>
+</div>
+</div>
+	  <!-- feature end -->
 
-				   </div>
-				</div>
-			 </div>
-					
+	<!-- Why start -->
+	<h2 class="headspan text-center">Why </h2>
+
+
+	<div class="row align-items-center">
+	  <div class="col-md-6">
+		  <div class="entry">
+			  <img src="img/aboutus.png"  alt="">
+		  </div>
+	  </div>
+	  <div class="col-md-6">
+		  <div class="entry">
+			  <h2>We grow brands beyond set targets</h2>
+			  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam labore saepe sit impedit obcaecati deserunt sunt dolor aliquid excepturi quae modi, explicabo sapiente? Molestias debitis fuga earum ipsum. Quaerat quibusdam eos, voluptates placeat inventore quis animi explicabo rerum quia, perferendis omnis. Asperiores aut saepe sapiente neque natus rerum culpa molestiae!</p>
+		  </div>
+	  </div>
+  </div>
+
+
+  <div class="row align-items-center">
+	  <div class="col-md-6 order-md-2">
+		  <div class="entry">
+			  <img src="img/aboutus.png"  alt="">
+		  </div>
+	  </div>
+	  <div class="col-md-6 order-md-1">
+		  <div class="entry">
+			  <h2>We grow brands beyond set targets</h2>
+			  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam labore saepe sit impedit obcaecati deserunt sunt dolor aliquid excepturi quae modi, explicabo sapiente? Molestias debitis fuga earum ipsum. Quaerat quibusdam eos, voluptates placeat inventore quis animi explicabo rerum quia, perferendis omnis. Asperiores aut saepe sapiente neque natus rerum culpa molestiae!</p>
+		  </div>
+	  </div>
+  </div>
+
+	<!-- Why end -->
+
+   
+<!-- FAQ start -->
+<h2 class="headspan text-center">FAQ</h2>
+
+
+<div id="accordion">
+   <div class="card">
+	  <div class="card-header" id="headingOne">
+		 <h5 class="mb-0">
+			<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+			Collapsible Group Item #1
+			</button>
+		 </h5>
+	  </div>
+	  <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+		 <div class="card-body">
+			Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+		 </div>
+	  </div>
+   </div>
+   <div class="card">
+	  <div class="card-header" id="headingTwo">
+		 <h5 class="mb-0">
+			<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+			Collapsible Group Item #2
+			</button>
+		 </h5>
+	  </div>
+	  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+		 <div class="card-body">
+			Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+		 </div>
+	  </div>
+   </div>
+   <div class="card">
+	  <div class="card-header" id="headingThree">
+		 <h5 class="mb-0">
+			<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+			Collapsible Group Item #3
+			</button>
+		 </h5>
+	  </div>
+	  <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+		 <div class="card-body">
+			Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+		 </div>
+	  </div>
+   </div>
+</div>
+	  <!-- FAQ end -->
+
+
+<!-- testimonial start -->
+<h2 class="headspan text-center">Testimonials</h2>
+
+   <div class="row align-items-center">
+	  <div class="col-md-4 text-center ">
+		 <div class="entry" data-aos="zoom-in" data-aos-duration="2000">
+			<i class="far fa-user-circle"></i>
+			<p>Lorem ipsum dolor sit consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper</p>
+			<h5>John Doe</h5>
+		 </div>
+	  </div>
+	  <div class="col-md-4 text-center">
+		 <div class="entry" data-aos="zoom-in" data-aos-duration="2000">
+			<i class="far fa-user-circle"></i>
+			<p>Lorem ipsum dolor sit consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper</p>
+			<h5>Ngolo</h5>
+		 </div>
+	  </div>
+	  <div class="col-md-4 text-center">
+		 <div class="entry" data-aos="zoom-in" data-aos-duration="2000">
+			<i class="far fa-user-circle"></i>
+			<p>Lorem ipsum dolor sit consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper</p>
+			<h5>Konate</h5>
+		 </div>
+	  </div>
+   </div>
+</div>
+
+   <!-- testimonial end -->
+
 				
 
 				
